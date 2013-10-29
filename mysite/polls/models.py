824cc77b382004c609c0a0dd89ff6e24114c1d22
Maciejfsafew <maciejmazurx@gmail.com>
2013-10-29 16:32:33 -0700

    Polls model: Poll and Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTiemField('date published')
+
+class Choice(models.Model):
+    poll = models.ForeigKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
