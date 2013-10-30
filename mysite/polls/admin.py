3f67d5a5134e21e2999c8727bb5055b288b1b214
Maciejfsafew <maciejmazurx@gmail.com>
2013-10-29 17:59:46 -0700

    Adjusted Polls view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,11 +3,17 @@ from polls.models import Poll
 from polls.models import Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_today')
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
